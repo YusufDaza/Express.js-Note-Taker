1e112d7e59a15f4e4ea8d81c730e435de75f2edf
Joseph Daza <josephdazz@hotmail.com>
2024-03-07 18:32:49 -0500

    updated

---
+++ routes/notes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router()
+const fs = require("fs")  
+const path=require("path")
+//http://localhost:3001/api/notes/
+router.get("/", (req, res) => {
+    fs.readFile("./db/db.json","utf8", (err, data) => {
+        const newData = JSON.parse(data)
+        res.json(newData)
+    })
+})
+
+router.post("/", (req, res) => {
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        const newData = JSON.parse(data)
+        newData.push(req.body)
+        res.json(newData)
+    })
+})
+
+module.exports=router
--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-
+const PORT=process.env.PORT||3001
 // modularized(put in separate files) the api routes
 //http://localhost:3001
 app.use("/api",route)
 
-app.get("api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 //api routes
